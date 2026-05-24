039481978afdcfc9f35ac6c8e556764e0c275554
abel1502 <terragame.abel@gmail.com>
2026-05-24 21:27:41 +0200

    Rework sentinel a bit more

--- src/nanotable/field.py
+++ src/nanotable/field.py
@@ -7,20 +7,24 @@ from typing_extensions import Sentinel
 MISSING = Sentinel("MISSING")
 
 
-# Not putting `| MISSING` in the return type because neither mypy nor pylance understand it. This will have to do
-type FieldGetter[Obj] = typing.Callable[[Obj, str], typing.Any | Sentinel]
+# Can't put `MISSING` in type annotations because neither mypy nor pylance understand it. This will have to do
+type typeof_MISSING = Sentinel
 
 
-def attr_getter(obj: object, key: str) -> typing.Any | Sentinel:
+type FieldGetter[Obj] = typing.Callable[[Obj, str], typing.Any | typeof_MISSING]
+
+
+def attr_getter(obj: object, key: str) -> typing.Any | typeof_MISSING:
     return getattr(obj, key, MISSING)
 
 
-def dict_getter(obj: dict[str, typing.Any], key: str) -> typing.Any | Sentinel:
+def dict_getter(obj: dict[str, typing.Any], key: str) -> typing.Any | typeof_MISSING:
     return obj.get(key, MISSING)
 
 
 __all__ = [
     "MISSING",
+    "typeof_MISSING",
     "FieldGetter",
     "attr_getter",
     "dict_getter",
