b8491848bc33f75eea717b31e67d2725454fd4d8
abel1502 <terragame.abel@gmail.com>
2026-05-27 11:53:03 +0200

    More Table tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -6,6 +6,8 @@ import pytest
 
 import nanotable.table
 from nanotable.table import Table
+import nanotable.field
+from nanotable.errors import PrimaryIndexError
 
 
 def test_exports() -> None:
@@ -45,4 +47,49 @@ class TestTable:
         
         table.clear()
         assert len(table) == 0
+    
+    def test_getfield_options(self) -> None:
+        t = Table[typing.Any]()
+        assert t._getfield_factory is nanotable.field.getfield_attr
+        
+        t = Table(of_objects=True)
+        assert t._getfield_factory is nanotable.field.getfield_attr
+        
+        t = Table(of_dicts=True)
+        assert t._getfield_factory is nanotable.field.getfield_item
+        
+        t = Table(getfield_factory=lambda name: lambda obj: 123)
+        assert t._getfield_factory("foo")(None) == 123
+    
+    def test_of_objects_by_default(self) -> None:
+        table = Table[MyObject]()\
+            .primary_index_on("id")
+        
+        table.add(MyObject(1, "Foo"))
+
+        assert table[1].name == "Foo"
+        
+        with pytest.raises(ValueError, match=r"[\'\"]id[\'\"]"):
+            table.add({"id": 2, "name": "Bar"})  # type: ignore
+
+    def test_primary_index(self) -> None:
+        table = Table[MyObject]()
+        
+        table.add(MyObject(1, "Foo"))
+        
+        assert not table.has_primary_index
+        
+        with pytest.raises(PrimaryIndexError):
+            table[1]
+        
+        table.primary_index_on("id")
+        
+        assert table.has_primary_index
+        
+        assert table[1].name == "Foo"
+        assert table.primary_index[1].name == "Foo"
+        assert table.by.id[1].name == "Foo"
+        
+        with pytest.raises(ValueError, match=r"[\'\"]id[\'\"]"):
+            table.add({"id": 2, "name": "Bar"})
 
